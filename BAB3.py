44ada1fdc59fcfd35636a2d74878de8b2b1eb244
sriddayo <sridadyob@gmail.com>
2026-08-05 12:39:02 +0700

    Add files via upload

---
+++ BAB3.py
@@ -0,0 +1,20 @@
+Nama = "sridadyo burhannudin yusuf N"
+umur = "25"
+tinggi = "160 cm"
+lulus = "True"
+
+print("Nama saya: " , Nama)
+print("Umur saya: " , umur)
+print("Tinggi saya: " , tinggi)
+print("lulus saya: " , lulus)
+
+
+produk ="sepatu lari"
+Stok =15
+Harga = 249.5
+Tersedia ="True"
+print ("Nama produk:" ,produk)
+print ("Stok yang tersedia:" , Stok)
+print ("Harga produk ($): " , Harga)
+print ("barang tersedia di toko:" , Tersedia)
+print ("stok di tambah harga:" , Stok + Harga)
