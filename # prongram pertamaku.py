dce096ff3383fcb7a4f2ffdf43cb9ece6df08d70
sriddayo <sridadyob@gmail.com>
2026-08-05 08:16:30 +0700

    Add files via upload

---
+++ # prongram pertamaku.py
@@ -0,0 +1,21 @@
+# prongram pertamaku
+# nama:Sridadyo burhannudin Y.N
+# kelas: x RPL,
+print()
+print("===========================")
+print("__SELAMAT_DATANG_DI_PYTHON!")
+print("===========================")
+print()
+print("prongram_ini_dibuat_oleh", "sridadyo burhannudin y.n")
+print("sekolah:_SMK_TJP_Tuban")
+print("Tahun_ :", 2024)
+print ( 10 + 5)    #penjumlahan
+print (20 - 8)     #pengurangan
+print ( 4 * 7)     #perkalian
+print ( 15 / 4)    #pembagian 
+print ( 15 // 4)   #pembagian bulat
+print ( 15 % 4)    #sisia bagi
+print (2 ** 8)     #perpangkatan
+a = 24
+b = 7
+print ("penjumlahan:", a + b)
